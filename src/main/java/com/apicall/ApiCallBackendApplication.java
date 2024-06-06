f6b281eba00bd004acd3bc09d10e6a3beea82c10
Dharmender <Dharmender@ALFA-Dharmender-Kumar>
2024-06-07 00:41:10 +0530

    added new conf for war

--- src/main/java/com/apicall/ApiCallBackendApplication.java
+++ src/main/java/com/apicall/ApiCallBackendApplication.java
@@ -2,10 +2,16 @@ package com.apicall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaRepositories
+@EnableWebMvc
+@EnableSpringDataWebSupport
 public class ApiCallBackendApplication {
 
 	public static void main(String[] args) {
